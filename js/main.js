c665c73f9c3e237409d9a323c2eeb9af62621762
jkoval <jack@jackkoval.co.uk>
2015-04-25 18:28:35 +0100

    Added main js file
    Started adding the main JS file which will include all the main
    functionality of the program along with the "in" point of the program

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Main JavaScript file for the game.
+
+// Let's just declare a variable for our canvas up here so that it can be used everywhere.
+// Dunno much about JavaScript scope but oh well.
+var canvas;
+
+// I'll do the same with the context for drawing. We can then pass this variable around to
+// different .js files that need it
+var graphics;
+
+// Declare some variables for the size of the canvas that we will be working with. Its easier to
+// just use width and height rather than having to type canvas.width, canvas.height each time.
+var width, height;
+
+// Here is an Init function where everything will happen when the game first runs. This should be
+// one of the only scripts we call from the html file itself.
+function init() {
+	
+	canvas = document.getElementById('drawSurface');
+	graphics = canvas.getContext('2d');
+	
+	width = canvas.width;
+	height = canvas.height;
+	
+}
